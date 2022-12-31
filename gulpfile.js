308e84de4d62184b212512c4f58b3efb33cb003b
Efren <histrix-dev@gmail.com>
2022-12-30 22:18:12 -0600

    The festival section is ready, aditional, it is added new extensions of image (.webp & .avif) for the optimization of the site

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,57 @@
-const {src, dest, watch} = require("gulp")
-const sass = require("gulp-sass")(require('sass'))
-const plumber = require('gulp-plumber')
+const {src, dest, watch, parallel} = require("gulp");
 
+// CSS
+const sass = require("gulp-sass")(require('sass'));
+const plumber = require('gulp-plumber');
+
+// Image
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif =  require('gulp-avif');
 
 function css( done ) {
   src("src/scss/**/*.scss").pipe(plumber())
                            .pipe(sass()) // Compile SASS file
-                           .pipe(dest("build/css")) // save into HardDisk
-  done() //Call notify toa Gulp when the it's the final
+                           .pipe(dest("build/css")); // save into HardDisk
+  done(); //Call notify toa Gulp when the it's the final
+}
+
+function imageMin( done ) {
+  const options = {
+    optimizationLevel: 3
+  }
+  src('src/img/**/*.{png,jpg}').pipe(cache(imagemin()))
+                                 .pipe(dest('build/img'));
+  done();
+}
+
+function versionWebp( done ) {
+  const options = {
+    quality: 50
+  };
+
+  src('src/img/**/*.{png,jpg}').pipe(webp(options))
+                               .pipe(dest('build/img'));
+  done();
+}
+
+function versionAvif( done ) {
+  const options = {
+    optimizationLevel: 3
+  }
+  src('src/img/**/*.{png,jpg}').pipe(avif(options))
+                               .pipe(dest('build/img'));
+  done();
 }
 
 function dev( done ) {
-  watch("src/scss/**/*.scss", css)
-
-  done()
+  watch("src/scss/**/*.scss", css);
+  done();
 }
 
-exports.css = css
-exports.dev = dev
+exports.css = css;
+exports.imageMin = imageMin;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev =  parallel( imageMin, versionWebp, versionAvif, dev);
