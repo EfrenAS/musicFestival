15d5fb82911f02716d0ef84059364ce7e20b183a
Efren <histrix-dev@gmail.com>
2023-01-05 21:00:57 -0600

    The site is finished! Added functions for minify the files .css and .js with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,11 @@ const {src, dest, watch, parallel} = require("gulp");
 // CSS
 const sass = require("gulp-sass")(require('sass'));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+
 
 // Image
 const cache = require('gulp-cache');
@@ -10,9 +15,21 @@ const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif =  require('gulp-avif');
 
+// Javascript
+
+const terser = require('gulp-terser-js');
+
 function css( done ) {
-  src("src/scss/**/*.scss").pipe(plumber())
+  const pluggins = [
+    autoprefixer(),
+    cssnano()
+  ];
+
+  src("src/scss/**/*.scss").pipe(sourcemaps.init())
+                           .pipe(plumber())
                            .pipe(sass()) // Compile SASS file
+                           .pipe(postcss(pluggins))
+                           .pipe(sourcemaps.write('.'))
                            .pipe(dest("build/css")); // save into HardDisk
   done(); //Call notify toa Gulp when the it's the final
 }
@@ -46,7 +63,10 @@ function versionAvif( done ) {
 }
 
 function javascript ( done ) {
-  src('src/js/**/*.js').pipe( dest('build/js') );
+  src('src/js/**/*.js').pipe(sourcemaps.init())
+                       .pipe( terser() )
+                       .pipe(sourcemaps.write('.'))
+                       .pipe( dest('build/js') );
   done();
 }
 
@@ -61,4 +81,4 @@ exports.js = javascript;
 exports.imageMin = imageMin;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.dev =  parallel( imageMin, versionWebp, versionAvif, javascript, dev);
+exports.dev =  parallel(imageMin, versionWebp, versionAvif, javascript, dev);
